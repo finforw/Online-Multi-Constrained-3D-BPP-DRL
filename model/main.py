6d8e61929490ca14ee4fa9dce82532696b4eaf59
Tieyong Yu <tieyongwealth@gmail.com>
2026-01-19 12:08:09 -0800

    bump initial psi

--- model/main.py
+++ model/main.py
@@ -165,7 +165,7 @@ def train_actor_critic(model, optimizer, criterion, env, n_episodes=2000,
     total_global_steps = 0
     # totals = []
     model.train()
-    initial_psi = 0.1  # Start very high
+    initial_psi = 0.15  # Start very high
     final_psi = 0.01   # End low to allow convergence
     decay_horizon = 249000
     best_val_score = -float('inf')
