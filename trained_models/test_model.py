d830c58c36a589624d7f7072b5ed4d6519dc97ef
Tieyong Yu <tieyongwealth@gmail.com>
2026-02-16 10:21:20 -0800

    Add model artifacts

--- trained_models/test_model.py
+++ trained_models/test_model.py
@@ -134,7 +134,7 @@ def calc_space_utilization(placed_items, bin_size=1000):
 if __name__ == "__main__":
     # Example Usage
     evaluate_model(
-        model_path="trained_models/golden/model.pt",
-        dataset_path="test_data/cut_1.pt",
+        model_path="tmp/model1.pt",
+        dataset_path="test_data/random.pt",
         device="cuda" if torch.cuda.is_available() else "cpu"
     )
