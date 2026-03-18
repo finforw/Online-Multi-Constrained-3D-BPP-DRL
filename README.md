cea3e746e06391dcb71e5f622a617fee461aaae8
Tieyong Yu <tieyongwealth@gmail.com>
2026-03-17 23:54:34 -0700

    Add eta-only constraint test results

--- README.md
+++ README.md
@@ -57,6 +57,11 @@ Average Reward: 7.951
 Average Utilization: 69.590%
 Average COG Distance: 1.453
 
+Hard constraints on ETA:
+Average Reward: 6.910
+Average Utilization: 59.106%
+Average COG Distance: 1.638
+
 Soft constraints on COG plus hard constraints on ETA:
 Average Reward: 6.836
 Average Utilization: 58.833%
--- trained_models/test_model.py
+++ trained_models/test_model.py
@@ -161,8 +161,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     evaluate_model(
-        model_path="trained_models/ours/vanilla.pt",
-        dataset_path="test_data/cut_1_expanded.pt",
+        model_path="trained_models/ours/eta/eta.pt",
+        dataset_path="test_data/fizz_fuzz.pt",
         args=args,
         device="cuda" if torch.cuda.is_available() else "cpu"
     )
